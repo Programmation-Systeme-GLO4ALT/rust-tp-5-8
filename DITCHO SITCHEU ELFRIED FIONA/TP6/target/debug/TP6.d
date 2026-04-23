947361b983bfbd88f246c224cc55fd2dd189f6f3
fiona elfried <elfriedfiona@gmail.com>
2026-04-23 01:58:39 +0100

    Add new files and incremental changes for TP6
    - Created work-products.bin for incremental build artifacts.
    - Added lock file for incremental compilation.
    - Introduced tp6_data.txt with sample data entries.
    - Added tp6_poeme.txt containing a short poem.
    - Created tp6_test_unit.txt with basic test phrases.

---
+++ DITCHO SITCHEU ELFRIED FIONA/TP6/target/debug/TP6.d
@@ -0,0 +1,1 @@
+D:\coursN4S2\programmation_systeme\rust-tp-5-8\DITCHO\ SITCHEU\ ELFRIED\ FIONA\TP6\target\debug\TP6.exe: D:\coursN4S2\programmation_systeme\rust-tp-5-8\DITCHO\ SITCHEU\ ELFRIED\ FIONA\TP6\src\main.rs
