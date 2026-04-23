a217b3427cd0580df687615e6f4fa1ebe7db85cd
fiona elfried <elfriedfiona@gmail.com>
2026-04-23 01:38:47 +0100

    Add generated files and configuration for TP5
    - Created new binary work-products file for incremental compilation.
    - Added lock file for incremental build management.
    - Introduced configuration file (tp5_config.conf) with version and API URL settings.

---
+++ DITCHO SITCHEU ELFRIED FIONA/TP5/target/debug/build/thiserror-3f683beffd42193e/build_script_build-3f683beffd42193e.d
@@ -0,0 +1,5 @@
+D:\coursN4S2\programmation_systeme\rust-tp-5-8\DITCHO SITCHEU ELFRIED FIONA\TP5\target\debug\build\thiserror-3f683beffd42193e\build_script_build-3f683beffd42193e.d: C:\Users\DELL\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-1.0.69\build.rs
+
+D:\coursN4S2\programmation_systeme\rust-tp-5-8\DITCHO SITCHEU ELFRIED FIONA\TP5\target\debug\build\thiserror-3f683beffd42193e\build_script_build-3f683beffd42193e.exe: C:\Users\DELL\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-1.0.69\build.rs
+
+C:\Users\DELL\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-1.0.69\build.rs:
